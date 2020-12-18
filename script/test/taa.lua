42d0a09539df8eef710f1806278c613dc01949e2
guomeimei <838181680@qq.com>
2020-12-18 23:35:04 +0800

    fxaa

--- script/test/taa.lua
+++ script/test/taa.lua
@@ -5,22 +5,35 @@ local mesh = Mesh.CreteMeshFormSimpleConcavePolygon(vertices, LColor.new(300, 10
 
 mesh.shader = Shader.GetBaseShader()
 mesh.transform:scale(4, 4)
-mesh.transform:moveTo(50, 50)
+mesh.transform:moveTo(20,2)
 
-local settings = {type  = "2d", format="rgba16f", msaa=0,  mipmaps="none"}
-local texturesize = 512
+local settings = {type  = "2d", format="rgba8", msaa=0,  mipmaps="none"}
 local downsamples = {}
 
-local downsamplesize = texturesize / 4
 
-local canvas = Canvas.new(texturesize, texturesize, settings)
-
+local canvas = Canvas.new(love.graphics.getPixelWidth(), love.graphics.getPixelHeight(), settings)
+local meshquad = _G.MeshQuad.new(love.graphics.getWidth(), love.graphics.getHeight(), LColor.new(255, 255, 255, 255), canvas)
+meshquad.shader = Shader.GetFXAAShader(canvas:getWidth(), canvas:getHeight())
+local str = "FXAA key: Space"
 app.render(function(dt)
-    -- local mode, alphamode = love.graphics.getBlendMode( )
-    -- _G.pushCanvas(canvas)
+    
+    _G.pushCanvas(canvas)
     mesh:draw()
-    -- meshhelper:draw()
-    -- _G.popCanvas()
+    _G.popCanvas()
+    meshquad:draw()
+    love.graphics.print(str, 10, 20)
+end)
 
-  
+local shaderindex = 0
+app.keypressed(function(key, scancode, isrepeat)
+    if key == "space" then
+        shaderindex = shaderindex + 1
+        if shaderindex % 2 == 0 then
+            str = "FXAA key: Space"
+            meshquad.shader = Shader.GetFXAAShader(canvas:getWidth(), canvas:getHeight())
+        else
+            str = "No FXAA key: Space"
+            meshquad.shader = Shader.GetBaseShader()
+        end
+    end
 end)
