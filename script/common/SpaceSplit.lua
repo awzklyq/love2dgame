3e3a414127e64f3c8bf087df3f04edcc78c382d4
awzklyq <awzklyq@163.com>
2022-12-04 20:16:58 +0800

    Split Space

--- script/common/SpaceSplit.lua
+++ script/common/SpaceSplit.lua
@@ -1,6 +1,11 @@
 _G.SpaceSplit = {}
 
-_G.SpaceSplit.SplitBatch = function(VertsAABB, targetSize, splitGranularity)
+SpaceSplit.TypeBySurface = 1
+SpaceSplit.TypeByAix = 2
+SpaceSplit.TypeByHalf = 2
+
+SpaceSplit.Type = SpaceSplit.TypeByAix
+_G.SpaceSplit.SplitBatchBySurface = function(VertsAABB, targetSize, splitGranularity)
     local bestCost = math.maxFloat
     local bestAxis = -1;
     local bestIndex = -1;
@@ -72,6 +77,130 @@ _G.SpaceSplit.SplitBatch = function(VertsAABB, targetSize, splitGranularity)
 end
 
 
+_G.SpaceSplit.SplitBatchByAix = function(VertsAABB, targetSize, splitGranularity)
+    local bestCost = math.maxFloat
+    local bestAxis = -1;
+    local bestIndex = -1;
+
+    local numv = #VertsAABB
+
+    local aabb = BoundBox.new()
+    for i = 1, numv do
+        aabb = aabb + VertsAABB[i]
+    end
+
+    bestAxis = 1
+    local AxiBoxExt = aabb.max - aabb.min
+    local TheBestAix = AxiBoxExt.x
+
+    if AxiBoxExt.y > TheBestAix then
+        TheBestAix = AxiBoxExt.y
+        bestAxis = 2
+    end
+
+    if AxiBoxExt.z > TheBestAix then
+        TheBestAix = AxiBoxExt.z
+        bestAxis = 3
+    end 
+
+    table.sort(VertsAABB, function(a, b)
+        if bestAxis == 1 then
+            return a.center.x < b.center.x
+        elseif bestAxis == 2 then
+            return a.center.y < b.center.y
+        elseif bestAxis == 3 then
+            return a.center.z < b.center.z
+        else
+            assert(false)
+            return false
+        end
+    end)
+
+    local LeftBoxs = {}      
+    local LeftBoundBox = BoundBox.new()
+    for i = 1, numv, 1 do
+        LeftBoundBox = LeftBoundBox + VertsAABB[i]
+        LeftBoxs[#LeftBoxs + 1] = LeftBoundBox
+    end
+
+    local RightBoxs = {}
+    local RightBoundBox = BoundBox.new()
+    for i = numv, 1, -1 do
+        RightBoundBox = RightBoundBox + VertsAABB[i]
+        RightBoxs[i] = RightBoundBox
+    end
+
+    for i = splitGranularity, numv - splitGranularity, splitGranularity do
+        local countLeft = i
+        local countRight = numv - i;
+
+        local areaLeft = LeftBoxs[i]:GetSurfaceArea() * countLeft
+        local areaRight = RightBoxs[i + 1]:GetSurfaceArea() * countRight
+
+        local cost = areaLeft + areaRight
+        if cost < bestCost then
+            bestCost = cost;
+            bestIndex = i
+        end
+    end
+
+
+    
+    if bestIndex == -1 then
+        bestIndex = numv;
+    end
+
+    return bestIndex
+end
+
+_G.SpaceSplit.SplitBatchByAixHalf = function(VertsAABB, targetSize, splitGranularity)
+    local numv = #VertsAABB
+    if numv < targetSize or umv < splitGranularity then
+        return numv
+    end
+
+    local bestAxis = -1;
+    local bestIndex = -1;
+
+    local aabb = BoundBox.new()
+    for i = 1, numv do
+        aabb = aabb + VertsAABB[i]
+    end
+
+    bestAxis = 1
+    local AxiBoxExt = aabb.max - aabb.min
+    local TheBestAix = AxiBoxExt.x
+
+    if AxiBoxExt.y > TheBestAix then
+        TheBestAix = AxiBoxExt.y
+        bestAxis = 2
+    end
+
+    if AxiBoxExt.z > TheBestAix then
+        TheBestAix = AxiBoxExt.z
+        bestAxis = 3
+    end 
+
+    table.sort(VertsAABB, function(a, b)
+        if bestAxis == 1 then
+            return a.center.x < b.center.x
+        elseif bestAxis == 2 then
+            return a.center.y < b.center.y
+        elseif bestAxis == 3 then
+            return a.center.z < b.center.z
+        else
+            assert(false)
+            return false
+        end
+    end)
+    
+    bestIndex = math.floor(numv / 2)
+    
+    return bestIndex
+end
+
+
+
 _G.SpaceSplit.SplitTable = function(VertsAABB, index1, index2)
     assert(index1 > 0 and index2 <= #VertsAABB)
     local result = {}
@@ -82,14 +211,24 @@ _G.SpaceSplit.SplitTable = function(VertsAABB, index1, index2)
 end
 
 _G.SpaceSplit.GenerateBatchesRecursive = function(VertsAABB, targetSize, splitGranularity, Results)
-    local SplitIndex = SpaceSplit.SplitBatch(VertsAABB, targetSize, splitGranularity)
+    local SplitIndex
 
+    if SpaceSplit.Type == SpaceSplit.TypeBySurface then
+        SplitIndex = SpaceSplit.SplitBatchBySurface(VertsAABB, targetSize, splitGranularity)
+    elseif  SpaceSplit.Type == SpaceSplit.TypeByAix then
+        SplitIndex = SpaceSplit.SplitBatchByAix(VertsAABB, targetSize, splitGranularity)
+    elseif  SpaceSplit.Type == SpaceSplit.TypeByHalf then
+        SplitIndex =  SpaceSplit.SplitBatchByAixHalf(VertsAABB, targetSize, splitGranularity)
+    else
+        SplitIndex = SpaceSplit.SplitBatchBySurface(VertsAABB, targetSize, splitGranularity)
+    end
     if SplitIndex == #VertsAABB then
         -- Results[#Results + 1] = VertsAABB
         return
     end
 
     local Table1 = _G.SpaceSplit.SplitTable(VertsAABB, 1, SplitIndex)
+    
     if SplitIndex + 1 <= targetSize then
         Results[#Results + 1] = Table1
     else
--- script/test/test_SpaceSplit.lua
+++ script/test/test_SpaceSplit.lua
@@ -4,7 +4,7 @@ FileManager.addAllPath("assert")
 local width = love.graphics.getPixelWidth() 
 local height = love.graphics.getPixelHeight()
 
-local meshobj = Mesh3D.new("SM_RailingStairs_Internal.obj")
+local meshobj = Mesh3D.new("S_BuildingSetA_Tree_02.obj")
 meshobj:setBaseColor(LColor.new(125,125,125, 255))
 
 
@@ -26,33 +26,66 @@ for i = 1, #verts - 1, 3 do
     vertsAABB[#vertsAABB + 1] =  meshobj.transform3d:mulBoundBox(aabb)
     
 end
-local Results = SpaceSplit.GenerateBatches(vertsAABB, 64, 2)
-local DrawBoxs = {}
 
+SpaceSplit.TypeBySurface = 1
+SpaceSplit.TypeByAix = 2
 
-log('aaaa', #Results)
+SpaceSplit.Type = SpaceSplit.TypeBySurface
+
+local Results = SpaceSplit.GenerateBatches(vertsAABB, 512, 4)
+local DrawBoxsBySurface = {}
+
 
 for i = 1, #Results do
-    log("bbbbbb", #Results[i])
     local ab = BoundBox.new()
     for j = 1, #Results[i] do
         ab = ab + Results[i][j]
     end
 
     local ss = ab.max - ab.min
-    log('ssssssss', ss.x, ss.y, ss.z)
-    DrawBoxs[#DrawBoxs +1] = ab:buildMeshLines()
-    DrawBoxs[#DrawBoxs]:setBGColor(LColor.new(math.random(80,255), math.random(80,255), math.random(80,255), 255))
+    DrawBoxsBySurface[#DrawBoxsBySurface +1] = ab:buildMeshLines()
+    DrawBoxsBySurface[#DrawBoxsBySurface]:setBGColor(LColor.new(math.random(80,255), math.random(80,255), math.random(80,255), 255))
 end
+
+
+SpaceSplit.Type = SpaceSplit.TypeByAix
+
+Results = SpaceSplit.GenerateBatches(vertsAABB, 512, 4)
+local DrawBoxsByAix = {}
+
+
+for i = 1, #Results do
+    local ab = BoundBox.new()
+    for j = 1, #Results[i] do
+        ab = ab + Results[i][j]
+    end
+
+    local ss = ab.max - ab.min
+    DrawBoxsByAix[#DrawBoxsByAix +1] = ab:buildMeshLines()
+    DrawBoxsByAix[#DrawBoxsByAix]:setBGColor(LColor.new(math.random(80,255), math.random(80,255), math.random(80,255), 255))
+end
+
+SpaceSplit.Type = SpaceSplit.TypeByHalf
+
+Results = SpaceSplit.GenerateBatches(vertsAABB, 512, 4)
+local DrawBoxsByAixHalf = {}
+
+
+for i = 1, #Results do
+    local ab = BoundBox.new()
+    for j = 1, #Results[i] do
+        ab = ab + Results[i][j]
+    end
+
+    local ss = ab.max - ab.min
+    DrawBoxsByAixHalf[#DrawBoxsByAixHalf +1] = ab:buildMeshLines()
+    DrawBoxsByAixHalf[#DrawBoxsByAixHalf]:setBGColor(LColor.new(math.random(80,255), math.random(80,255), math.random(80,255), 255))
+end
+
 local aixs = Aixs.new(0,-100,-100, 500)
 local CanvasColor = Canvas.new(love.graphics.getPixelWidth(), love.graphics.getPixelHeight(), {format = "rgba8", readable = true, msaa = 0, mipmaps="none"})
 
-
-local GeneratDrawData = function()
-
-end
-
-
+local DrawType = 0
 
 -- local mab = meshobj.transform3d:mulBoundBox(BoundBox.buildFromMesh3D(meshobj))
 -- local mm = mab:buildMeshLines()
@@ -63,10 +96,21 @@ app.render(function(dt)
 
     meshobj:draw()
 
-    for i = 1, #DrawBoxs do
-        DrawBoxs[i]:draw()
+    if DrawType == 0 then
+        for i = 1, #DrawBoxsBySurface do
+            DrawBoxsBySurface[i]:draw()
+        end
+    elseif DrawType == 1 then
+        for i = 1, #DrawBoxsByAix do
+            DrawBoxsByAix[i]:draw()
+        end
+    else
+        for i = 1, #DrawBoxsByAixHalf do
+            DrawBoxsByAixHalf[i]:draw()
+        end
     end
 
+    love.graphics.print( " DrawType: "..  tostring(DrawType), 10, 10)
     -- boxlines:draw()
     -- mm:draw()
     RenderSet.ClearCanvasColorAndDepth()
@@ -76,6 +120,6 @@ end)
 
 app.keypressed(function(key, scancode, isrepeat)
     if key == "space" then
-        -- log('eye: ',currentCamera3D.eye.x, currentCamera3D.eye.y, currentCamera3D.eye.z)
+        DrawType = (DrawType + 1) % 3
     end
 end)
