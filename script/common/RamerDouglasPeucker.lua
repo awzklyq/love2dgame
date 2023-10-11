05d73c75fb4abf9d83611c870f6ac9b5be1308b5
awzklyq <awzklyq@163.com>
2023-10-11 20:29:57 +0800

    Ramer Douglas Peucker

---
+++ script/common/RamerDouglasPeucker.lua
@@ -0,0 +1,58 @@
+_G.RamerDouglasPeucker = {}
+RamerDouglasPeucker.ThresholdDistance = 10
+RamerDouglasPeucker.FindRedundanceData = function(Points, StartPointIndex, EndPointIndex, NeedDeleteTab)
+    if StartPointIndex + 1 >= EndPointIndex then
+        return false
+    end
+
+    local StartPoint = Points[StartPointIndex]
+    local EndPoint = Points[EndPointIndex]
+
+    local SelectIndex = -1
+    local MaxDistance = -1
+    for i = StartPointIndex + 1, EndPointIndex - 1 do
+        local p = Points[i]
+        local dis = math.PointToLineDistanceXY2D(p, StartPoint.x, StartPoint.y, EndPoint.x, EndPoint.y)
+
+        if dis > RamerDouglasPeucker.ThresholdDistance then
+            if MinDistance == -1 then
+                SelectIndex = i
+                MaxDistance = dis
+            elseif dis > MaxDistance then
+                SelectIndex = i
+                MaxDistance = dis
+            end
+        end
+    end
+
+    if SelectIndex == -1 then
+        for i = StartPointIndex + 1, EndPointIndex - 1 do
+            if NeedDeleteTab[i] ~= nil then
+                errorAssert(false, "NeedDeleteTab is not nil")
+            end
+
+            NeedDeleteTab[i] = i
+        end
+        return false
+    end
+
+    RamerDouglasPeucker.FindRedundanceData(Points, StartPointIndex, SelectIndex, NeedDeleteTab)
+    RamerDouglasPeucker.FindRedundanceData(Points, SelectIndex + 1, EndPointIndex, NeedDeleteTab)
+end
+
+RamerDouglasPeucker.Process2D = function(Points)
+    if #Points < 3 then
+        return Points
+    end
+
+    local NewPoints = {}
+    local NeedDeleteTab = {}
+    RamerDouglasPeucker.FindRedundanceData(Points, 1, #Points, NeedDeleteTab)
+    for i = 1, #Points do
+        if NeedDeleteTab[i] == nil then
+            NewPoints[#NewPoints + 1] = Vector.copy(Points[i])
+        end
+    end
+
+    return NewPoints
+end
