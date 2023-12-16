de83987586358f98141535da3f751cdb1571ef60
awzklyq <awzklyq@163.com>
2023-12-16 17:18:12 +0800

    dof

--- script/postprocess/watercolorfilter.lua
+++ script/postprocess/watercolorfilter.lua
@@ -7,8 +7,12 @@ WaterColorFilterNode.Canvae.renderHeight = 1
 WaterColorFilterNode.meshquad = _G.MeshQuad.new(1,1, LColor.new(255, 255, 255, 255))
 
 WaterColorFilterNode.Color = LColor.new(255,0,0,255);
-WaterColorFilterNode.Sigma = 4
+WaterColorFilterNode.Offset = 2
 
+WaterColorFilterNode.Type = 1
+
+WaterColorFilterNode.Scale1 = 0.8
+WaterColorFilterNode.Scale2 = 0.9
 WaterColorFilterNode.Execute = function(inimage)
    
     if WaterColorFilterNode.Canvae.renderWidth ~= inimage.renderWidth  or WaterColorFilterNode.Canvae.renderHeight ~= inimage.renderHeight then
@@ -22,7 +26,7 @@ WaterColorFilterNode.Execute = function(inimage)
     love.graphics.setCanvas(WaterColorFilterNode.Canvae.obj)
     love.graphics.clear()
     -- WaterColorFilterNode.meshquad:setCanvas(inimage)
-    WaterColorFilterNode.meshquad.shader = Shader.GetWaterColorFilterShader(inimage, WaterColorFilterNode.Canvae.renderWidth , WaterColorFilterNode.Canvae.renderHeight,2)
+    WaterColorFilterNode.meshquad.shader = Shader["GetWaterColorFilterShader" .. tostring(WaterColorFilterNode.Type)](inimage, WaterColorFilterNode.Canvae.renderWidth , WaterColorFilterNode.Canvae.renderHeight,WaterColorFilterNode.Offset, WaterColorFilterNode.Scale1, WaterColorFilterNode.Scale2)
     WaterColorFilterNode.meshquad:draw()
     love.graphics.setCanvas()
 
@@ -36,9 +40,9 @@ WaterColorFilterNode.Execute = function(inimage)
     return WaterColorFilterNode.Canvae
 end
 
-function Shader.GetWaterColorFilterShader(img, w, h, offset, blurnum, sigma)
+function Shader.GetWaterColorFilterShader1(img, w, h, offset)
     if Shader['WaterColorFilterShader'] then
-        Shader['WaterColorFilterShader']:SetWaterColorFilterShader(img, w, h, offset, blurnum, sigma)
+        Shader['WaterColorFilterShader']:SetWaterColorFilterShader(img, w, h, offset)
         return Shader['WaterColorFilterShader']
     end
     local pixelcode = [[
@@ -48,24 +52,35 @@ function Shader.GetWaterColorFilterShader(img, w, h, offset, blurnum, sigma)
     uniform float imgw;
     uniform float imgh;
 
+    vec2 Circle(float Start, float Points, float Point) 
+    {
+        float Rad = (3.141592 * 2.0 * (1.0 / Points)) * (Point + Start);
+        return vec2(sin(Rad), cos(Rad));
+    }
+
+
     vec4 effect( vec4 color, sampler2D tex, vec2 texture_coords, vec2 screen_coords )
     {
         vec2 coords = texture_coords;
         float stepx = offset / imgw;
         float stepy = offset / imgh;
 
-        vec3 Value1 = texture2D(baseimg, coords + vec2(stepx, stepy)).xyz;
-        vec3 Value2 = texture2D(baseimg, coords + vec2(-stepx, stepy)).xyz;
-        vec3 Value3 = texture2D(baseimg, coords + vec2(-stepx, -stepy)).xyz;
-        vec3 Value4 = texture2D(baseimg, coords + vec2(stepx, -stepy)).xyz;
+        vec2 invwh = vec2(stepx, stepy);
+        float Start = 2.0/8.0;
+	    float Scale = offset;
 
-        vec3 Value5 = texture2D(baseimg, coords + vec2(0, stepy)).xyz;
-        vec3 Value6 = texture2D(baseimg, coords + vec2(-stepx, 0)).xyz;
-        vec3 Value7 = texture2D(baseimg, coords + vec2(0, -stepy)).xyz;
-        vec3 Value8 = texture2D(baseimg, coords + vec2(stepx, 0)).xyz;
+        vec3 Value1 = texture2D(baseimg, coords +  Circle(Start, 8.0, 1.0) * invwh).xyz;
+        vec3 Value2 = texture2D(baseimg, coords +  Circle(Start, 8.0, 2.0) * invwh).xyz;
+        vec3 Value3 = texture2D(baseimg, coords +  Circle(Start, 8.0, 3.0) * invwh).xyz;
+        vec3 Value4 = texture2D(baseimg, coords +  Circle(Start, 8.0, 4.0) * invwh).xyz;
+
+        vec3 Value5 = texture2D(baseimg, coords +  Circle(Start, 8.0, 5.0) * invwh).xyz;
+        vec3 Value6 = texture2D(baseimg, coords +  Circle(Start, 8.0, 6.0) * invwh).xyz;
+        vec3 Value7 = texture2D(baseimg, coords +  Circle(Start, 8.0, 7.0) * invwh).xyz;
+        vec3 Value8 = texture2D(baseimg, coords +  Circle(Start, 8.0, 8.0) * invwh).xyz;
 	
         vec3 ret0 = max(Value1, Value2);
-        vec3 ret2 = max(Value1, Value2);
+        vec3 ret2 = max(Value3, Value4);
         vec3 ret3 = max(ret0, ret2);
 
         vec3 ret4 = max(Value5, Value6);
@@ -76,8 +91,7 @@ function Shader.GetWaterColorFilterShader(img, w, h, offset, blurnum, sigma)
         texcolor.xyz = max(ret6, max(texcolor.xyz, ret3));
         return texcolor;
     }
-]]
- 
+]] 
     local vertexcode = [[
     vec4 position( mat4 transform_projection, vec4 vertex_position )
     {
@@ -96,6 +110,110 @@ function Shader.GetWaterColorFilterShader(img, w, h, offset, blurnum, sigma)
     shader.vscode = vertexcode
     shader.pscode = pixelcode
     Shader['WaterColorFilterShader'] = shader
+    
+    shader:SetWaterColorFilterShader(img, w, h, offset)
+    return shader;
+end
+
+
+
+function Shader.GetWaterColorFilterShader2(img, w, h, offset, Scale1, Scale2)
+    if Shader['WaterColorFilterShader2_Scale1' .. tostring(Scale1) .. '_Scale2'..tostring(Scale2)] then
+        Shader['WaterColorFilterShader2_Scale1'.. tostring(Scale1) .. '_Scale2'..tostring(Scale2)]:SetWaterColorFilterShader(img, w, h, offset, Scale1, Scale2)
+        return Shader['WaterColorFilterShader2_Scale1'.. tostring(Scale1) .. '_Scale2'..tostring(Scale2)]
+    end
+    local pixelcode = [[
+    uniform float offset;
+    uniform float Scale1;
+    uniform float Scale2;
+    uniform sampler2D baseimg;
+    uniform float imgw;
+    uniform float imgh;]]
+
+    pixelcode = pixelcode .. _G.ShaderFunction.Luminance
+
+    pixelcode = pixelcode .. [[
+    vec2 Circle(float Start, float Points, float Point) 
+    {
+        float Rad = (3.141592 * 2.0 * (1.0 / Points)) * (Point + Start);
+        return vec2(sin(Rad), cos(Rad));
+    }
+
+
+    vec4 MaxW(vec4 v1, vec4 v2)
+    {
+        if (v1.w > v2.w)
+            return v1;
+        else
+            return v2;
+
+    }
+    vec4 effect( vec4 color, sampler2D tex, vec2 texture_coords, vec2 screen_coords )
+    {
+        vec2 coords = texture_coords;
+        float stepx = offset / imgw;
+        float stepy = offset / imgh;
+
+        vec2 invwh = vec2(stepx, stepy);
+        float Start = 2.0/8.0;
+	    float Scale = offset;
+
+        vec3 Value1 = texture2D(baseimg, coords + vec2(stepx, stepy)).xyz;
+        vec3 Value2 = texture2D(baseimg, coords + vec2(-stepx, stepy)).xyz;
+        vec3 Value3 = texture2D(baseimg, coords + vec2(-stepx, -stepy)).xyz;
+        vec3 Value4 = texture2D(baseimg, coords + vec2(stepx, -stepy)).xyz;
+
+        vec3 Value5 = texture2D(baseimg, coords + vec2(0, stepy * 2)).xyz;
+        vec3 Value6 = texture2D(baseimg, coords + vec2(-stepx * 2, 0)).xyz;
+        vec3 Value7 = texture2D(baseimg, coords + vec2(0, -stepy * 2)).xyz;
+        vec3 Value8 = texture2D(baseimg, coords + vec2(stepx * 2, 0)).xyz;
+	
+        vec4 w1 = vec4(Value1, Luminance(Value1) * Scale1);
+        vec4 w2 = vec4(Value2, Luminance(Value2) * Scale1);
+        vec4 w3 = vec4(Value3, Luminance(Value3) * Scale1);
+        vec4 w4 = vec4(Value4, Luminance(Value4) * Scale1);
+
+        vec4 w5 = vec4(Value5, Luminance(Value5) * Scale2);
+        vec4 w6 = vec4(Value6, Luminance(Value6) * Scale2);
+        vec4 w7 = vec4(Value7, Luminance(Value7) * Scale2);
+        vec4 w8 = vec4(Value8, Luminance(Value8) * Scale2);
+
+        
+
+        vec4 ret0 = MaxW(w1, w2);
+        vec4 ret2 = MaxW(w3, w4);
+        vec4 ret3 = MaxW(ret0, ret2);
+
+        vec4 ret4 = MaxW(w5, w6);
+        vec4 ret5 = MaxW(w7, w8);
+        vec4 ret6 = MaxW(ret4, ret5);
+        vec4 texcolor = texture2D(baseimg, coords);
+        texcolor.w = Luminance(texcolor.rgb);
+        texcolor = MaxW(texcolor, MaxW(ret6, ret3));
+        texcolor.a = 1.0;
+        return texcolor;
+    }
+]] 
+    local vertexcode = [[
+    vec4 position( mat4 transform_projection, vec4 vertex_position )
+    {
+        return transform_projection * vertex_position;
+    }
+]]
+    local shader = Shader.new(pixelcode, vertexcode);
+    shader.SetWaterColorFilterShader = function (obj, img, w, h, offset, Scale1, Scale2)
+        obj:sendValue('baseimg', img.obj);
+        obj:sendValue('imgw', w or img.w);
+        obj:sendValue('imgh', h or img.h);
+        obj:sendValue('Scale2', Scale2);
+        obj:sendValue('Scale1', Scale1);
+
+        obj:sendValue('offset', offset or 1);
+    end
+
+    shader.vscode = vertexcode
+    shader.pscode = pixelcode
+    Shader['WaterColorFilterShader2_Scale1'.. tostring(Scale1) .. '_Scale2'..tostring(Scale2)] = shader
     
     shader:SetWaterColorFilterShader(img, w, h, offset)
     return shader;
--- script/test/test_watercolorfilter.lua
+++ script/test/test_watercolorfilter.lua
@@ -1,10 +1,14 @@
 
-local meshquad = _G.MeshQuad.new( RenderSet.screenwidth,  RenderSet.screenheight, LColor.new(255, 255, 255, 255))
-meshquad.w = RenderSet.screenwidth
-meshquad.h = RenderSet.screenheight
+-- local meshquad = _G.MeshQuad.new( RenderSet.screenwidth,  RenderSet.screenheight, LColor.new(255, 255, 255, 255))
+-- meshquad.w = RenderSet.screenwidth
+-- meshquad.h = RenderSet.screenheight
 
 --  local image = ImageEx.new("OriBA.png")
-local image = ImageEx.new("yizi.png")
+-- local image = ImageEx.new("yizi.png")
+local image = ImageEx.new("itgongzuo.jpg")
+
+image.renderWidth = RenderSet.screenwidth
+image.renderHeight = RenderSet.screenheight
 --local image = ImageEx.new("xing.png")
 app.update(function(dt)
    
@@ -23,9 +27,11 @@ app.render(function(dt)
 end)
 
 app.resizeWindow(function(w, h)
-    meshquad = _G.MeshQuad.new( w,  h, LColor.new(255, 255, 255, 255))
-    meshquad.w = w
-    meshquad.h = h
+    -- meshquad = _G.MeshQuad.new( w,  h, LColor.new(255, 255, 255, 255))
+    -- meshquad.w = w
+    -- meshquad.h = h
+    image.renderWidth = RenderSet.screenwidth
+    image.renderHeight = RenderSet.screenheight
 end)
 
 
@@ -34,3 +40,40 @@ app.keypressed(function(key, scancode, isrepeat)
         IsRenderOri = not IsRenderOri
     end
 end)
+
+local checkb = UI.CheckBox.new( 10, 10, 20, 20, "IsRenderOri" )
+checkb.IsSelect = IsRenderOri
+checkb.ChangeEvent = function(Enable)
+    IsRenderOri = Enable
+end
+
+
+local checkb = UI.CheckBox.new( 10, 30, 20, 20, "Open:Type 1, Closed:Type 2" )
+checkb.IsSelect = WaterColorFilterNode.Type == 1 
+checkb.ChangeEvent = function(Enable)
+    if Enable then
+        WaterColorFilterNode.Type = 1
+    else
+        WaterColorFilterNode.Type = 2
+    end 
+end
+
+local scrollbar1 = UI.ScrollBar.new( 'Offset', 10, 60, 200, 40, 0.1, 10, 0.1)
+scrollbar1.Value = WaterColorFilterNode.Offset
+scrollbar1.ChangeEvent = function(v)
+    WaterColorFilterNode.Offset = v
+end
+
+local scrollbar2 = UI.ScrollBar.new( 'Scale1', 10, 100, 200, 40, 0.01, 1, 0.01)
+scrollbar2.Value = WaterColorFilterNode.Scale1
+scrollbar2.ChangeEvent = function(v)
+    WaterColorFilterNode.Scale1 = v
+end
+
+
+local scrollbar3 = UI.ScrollBar.new( 'Scale2', 10, 140, 200, 40, 0.01, 1, 0.01)
+scrollbar3.Value = WaterColorFilterNode.Scale2
+scrollbar3.ChangeEvent = function(v)
+    WaterColorFilterNode.Scale2 = v
+end
+
